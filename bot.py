73826e7754991102ccc21ed144f886c1377f3ae5
ulungrakhanda-cmd <ulung.rakhanda@gmail.com>
2025-11-25 15:06:50 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,14 @@
 print("Bot berhasil jalan dari GitHub Actions!")
+import requests
+import os
+
+API_KEY = os.getenv("OPENWEATHER_API")
+CITY = "Jakarta"
+
+def get_weather():
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={CITY}&appid={API_KEY}&units=metric"
+    r = requests.get(url)
+    data = r.json()
+    return f"Cuaca {CITY}: {data['main']['temp']}°C, {data['weather'][0]['description']}"
+
+print(get_weather())
