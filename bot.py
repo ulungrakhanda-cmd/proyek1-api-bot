7575ec0d5b871d74c9d70b4ab2e09fe2a6fedc38
ulungrakhanda-cmd <ulung.rakhanda@gmail.com>
2025-11-25 17:02:32 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,13 +1,18 @@
-import requests
 import os
+import requests
 
-API_KEY = os.getenv("OPENWEATHER_API")
 CITY = "Jakarta"
+API_KEY = os.getenv("OPENWEATHER_API_KEY")
 
 def get_weather():
-    url = f"https://api.openweathermap.org/data/2.5/weather?q={CITY}&appid={API_KEY}&units=metric"
-    r = requests.get(url)
-    data = r.json()
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={CITY}&appid={API_KEY}&units=metric"
+    res = requests.get(url)
+    data = res.json()
+
+    # cek kalau request gagal
+    if res.status_code != 200 or "main" not in data:
+        return f"Gagal ambil cuaca: {data.get('message', 'Unknown error')}"
+
     return f"Cuaca {CITY}: {data['main']['temp']}°C, {data['weather'][0]['description']}"
 
 print(get_weather())
